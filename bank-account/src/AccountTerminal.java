ddfa896e36bcdcaefb05b1bf440a7495360b57fe
EversonF <eversonaraujo589gmail.com>
2025-03-17 21:12:47 -0300

    feat: Enhance AccountTerminal to collect user input for account creation

--- bank-account/src/AccountTerminal.java
+++ bank-account/src/AccountTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class AccountTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Agência: ");
+        String agencyNumber = scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da Conta: ");
+        String accountNumber = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String accountHolder = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta: ");
+        double balance = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println(
+                " Olá " + accountHolder + " , sua conta foi criada com sucesso, e sua agência é " + agencyNumber
+                        + " , conta " + accountNumber + " e seu saldo " + balance + " já está disponivel para saque");
     }
 }
