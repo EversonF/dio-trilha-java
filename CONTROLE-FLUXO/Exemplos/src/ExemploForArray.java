6dc0bc5df646e14788d39734044bb4b1c03d33d5
EversonF <eversonaraujo589gmail.com>
2025-03-22 13:55:31 -0300

    feat: Add ExemploForArray class with example usage of for loop and array

---
+++ CONTROLE-FLUXO/Exemplos/src/ExemploForArray.java
@@ -0,0 +1,8 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "Ana", "Bia", "Caio", "Duda", "Eva" };
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("Aluno no indice x " + x + " e " + alunos[x]);
+        }
+    }
+}
